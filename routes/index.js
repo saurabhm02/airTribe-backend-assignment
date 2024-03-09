0ff6b8fb51148a5d1d372490a770607a863f26c5
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-03-10 04:28:49 +0530

    update routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const { signUpInstructor, loginInstructor } = require("../controllers/Instructor
 const { createCourse, updateCourse, getAllCourses } = require("../controllers/Course");
 const { registerLead, updateLeadStatus, searchLeads } = require("../controllers/Lead");
 const { createComment } = require("../controllers/Comment");
+const { auth } = require("../middlewares/auth");
 
 
 // Instructor Routes
@@ -12,16 +13,17 @@ router.post('/instructor/signup', signUpInstructor);
 router.post('/instructor/login', loginInstructor);
 
 // Course Routes
-router.post('/course', createCourse);
+router.post('/createCourse', auth, createCourse);
 router.put('/course/:courseId', updateCourse);
-router.get('/courses', getAllCourses);
+router.get('/getAllcourses', getAllCourses);
 
 // Lead Routes
-router.post('/lead', registerLead);
+router.post('/registerLead', registerLead);
 router.put('/lead/:leadId', updateLeadStatus);
-router.get('/leads', searchLeads);
+router.get('/searchLead', searchLeads);
 
 // Comment Routes
-router.post('/comment', createComment);
+router.post('/leads/:leadId/createComment', createComment);
+
 
 module.exports = router;
